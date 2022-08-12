7af0c3f401242246dd920473b4b300a0fd802ea9
woogie <jeonguk29@naver.com>
2022-08-12 17:53:34 +0900

    결재창 카카오 페이로 이동 및 적금 목표액수, 저금 액수 보이게 그리고 사용자 폰에 shared preferences 이용하여 데이터 저장 까지 구현함

--- Flutter_Water_Animation-main/Flutter_Water_Animation-main/lib/main.dart
+++ Flutter_Water_Animation-main/Flutter_Water_Animation-main/lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 void main() {
   runApp(
@@ -27,9 +29,55 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-int cigarette = 20;
+int cigarette_count = 0;
+int add_count = 0;
+int add_count2 = 0;
+int money_count = 1000000;
+
+saveData() async {
+  var storage = await SharedPreferences.getInstance();
+  cigarette_count = storage.getInt('cigarette');
+  if( storage.getInt('save_money') == null){
+    add_count = 0;
+  }
+  else{
+    add_count = storage.getInt('save_money');
+    money_count = 1000000 - add_count;
+    if(money_count < 0){
+      storage.setInt('save_money',0);
+    }
+  }
+}
+
+setData(int num) async {
+  var storage = await SharedPreferences.getInstance();
+  storage.setInt('cigarette', num);
+}
+
+
+setSave() async {
+  var storage = await SharedPreferences.getInstance();
+  if( storage.getInt('save_money') == null){
+    storage.setInt('save_money',4500);
+  }
+  else{
+    add_count = storage.getInt('save_money');
+    int n = storage.getInt('save_money');
+    storage.setInt('save_money',n + 4500);
+  }
+
+}
+
+
+
 
 class _HomeState extends State<Home> {
+  @override
+  void initState(){  // 앱 로드 될때 바로 GET 하고 싶음 그럼 이렇게
+    super.initState();
+    saveData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +88,7 @@ class _HomeState extends State<Home> {
         child: Center(
           child: Column(
             children: [
-              Text('남은개비 : $cigarette'),
+              Text('남은개비 : $cigarette_count'),
               Container(
                 padding: EdgeInsets.all(30),
                 child: Center(
@@ -75,11 +123,19 @@ class _HomeState extends State<Home> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             IconButton(icon: Icon(Icons.smoking_rooms), onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-            setState(() { //setState : 호출하면 ui를 변경하겠다라는 것
-              cigarette = cigarette - 1;
-            });
-            }),
+              if(cigarette_count > 0)
+                {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                  setState(() { //setState : 호출하면 ui를 변경하겠다라는 것
+                    cigarette_count = cigarette_count - 1;
+                    setData(cigarette_count);
+                  });
+                }
+              else{
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Save()));
+              }
+              }),
+
             IconButton(icon: Icon(Icons.add_circle_outline), onPressed: (){
               Navigator.push(context, MaterialPageRoute(builder: (context) => Save()));
               
@@ -111,17 +167,99 @@ class _SaveState extends State<Save> {
       body: Container(
         child: Row(
           children: [
+            Container(
+              child: Column(
+                children: [
+                  Text('목표액수 까지 : $money_count'),
+                  Text('저금액수 : $add_count'),
+                ],
+              ),
+            ),
             OutlinedButton.icon( onPressed: () async {
               // 브라우저를 열 링크
-              const url = 'https://google.com';
+              const url = 'https://www.kakaopay.com/';
               // 외부 브라우저 실행
               await launch(url, forceWebView: false, forceSafariVC: false);
+
+              showDialog(
+                barrierDismissible: false, //barrierDismissible - Dialog를 제외한 다른 화면 터치 x
+                context: context,
+                builder: (context){
+                  return Dialog(
+                      child: Container(
+                        height: 400, width: 330,
+                        //padding: EdgeInsets.all(10),
+
+                        child: Column(
+                            crossAxisAlignment:CrossAxisAlignment.start,
+                            children: [
+                        Container(
+                        padding: EdgeInsets.all(30),
+                        child: Text("양심적으로 4500원 본인 적금 계좌에 입금 하고 오세요 100만원 금방입니다 ",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30, 10, 0, 5),
+                        child:
+                        Text("진짜 입금 하셨습니까?",
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const  EdgeInsets.fromLTRB(30, 0, 0, 30),
+                        child: Container(
+                          //padding: EdgeInsets.fromLTRB(100, 0, 0, 30),
+
+                          height:1.0,
+                          width:270.0,
+                          color:Colors.black12,),
+                      ),
+                      Container(
+                        padding: const  EdgeInsets.fromLTRB(30, 0, 20, 30),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(onPressed: (){
+                              Navigator.pop(context); //열려있는 Dialog를 닫는 부분입니다.
+                            }, child: Text("Cansel")),
+
+                            TextButton(onPressed: (){
+                              Navigator.pop(context); // 열려있는 Dialog를 닫는 부분입니다.
+                              setState(() { //setState : 호출하면 ui를 변경하겠다라는 것
+                                cigarette_count = 20;
+                                setData(cigarette_count);
+                                setSave();
+                                saveData();
+                              });
+                            }, child: Text("OK")),
+                          ],
+                        ),
+                      )
+                  ],
+                  ),
+                  ),
+                  );
+                },
+              );
             },
-              icon: Icon(Icons.add, size: 18), label: Text("OUTLINED BUTTON"), )
+              icon: Icon(Icons.savings, size: 50, color: Color(0xffefe240)), label: Text("카카오페이 바로가기", style: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),),
+            ),
+
           ],
         ),
       ),
-      bottomNavigationBar: BottomAppBar( child: Text('하단바임 ㅅㄱ') ),
     );
   }
 }
